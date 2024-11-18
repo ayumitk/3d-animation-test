aa460bbbdf239e19daae0b139fb62d3b27522f6b
Ayumi Takahashi <ayumee528@gmail.com>
2024-11-18 10:35:02 -0800

    Revert "mouse Interactive"
    This reverts commit 2472319dadff8e8ba695caffe77a24253ad5b8b3.

--- src/components/home/Experience.jsx
+++ src/components/home/Experience.jsx
@@ -8,29 +8,29 @@ import { useEffect, useState, useRef } from "react";
 import { motion } from "framer-motion-3d";
 
 export default function Experience(props) {
-	const mouse = {
-		x: useMotionValue(0),
-		y: useMotionValue(0),
-	};
+	// const mouse = {
+	// 	x: useMotionValue(0),
+	// 	y: useMotionValue(0),
+	// };
 
-	const smoothMouse = {
-		x: useSpring(mouse.x, { stiffness: 75, damping: 100, mass: 3 }),
-		y: useSpring(mouse.y, { stiffness: 75, damping: 100, mass: 3 }),
-	};
+	// const smoothMouse = {
+	// 	x: useSpring(mouse.x, { stiffness: 75, damping: 100, mass: 3 }),
+	// 	y: useSpring(mouse.y, { stiffness: 75, damping: 100, mass: 3 }),
+	// };
 
-	const manageMouse = (e) => {
-		const { innerWidth, innerHeight } = window;
-		const { clientX, clientY } = e;
-		const x = clientX / innerWidth;
-		const y = clientY / innerHeight;
-		mouse.x.set(x);
-		mouse.y.set(y);
-	};
+	// const manageMouse = (e) => {
+	// 	const { innerWidth, innerHeight } = window;
+	// 	const { clientX, clientY } = e;
+	// 	const x = clientX / innerWidth;
+	// 	const y = clientY / innerHeight;
+	// 	mouse.x.set(x);
+	// 	mouse.y.set(y);
+	// };
 
-	useEffect(() => {
-		window.addEventListener("mousemove", manageMouse);
-		return () => window.removeEventListener("mousemove", manageMouse);
-	}, []);
+	// useEffect(() => {
+	// 	window.addEventListener("mousemove", manageMouse);
+	// 	return () => window.removeEventListener("mousemove", manageMouse);
+	// }, []);
 
 	const [lightIntensity, setLightIntensity] = useState(40);
 	const [colorIntensity, setColorIntensity] = useState(0);
