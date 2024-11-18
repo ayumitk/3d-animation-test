54f7631629dfb005e1594f0c2ccb154883ef8f07
Ayumi Takahashi <ayumee528@gmail.com>
2024-11-18 10:22:39 -0800

    delete gui pages

--- src/components/home/Background.jsx
+++ src/components/home/Background.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useRef } from "react";
 import styles from "./Background.module.css";
 import { motion } from "framer-motion";
--- src/components/home/Experience.jsx
+++ src/components/home/Experience.jsx
@@ -8,29 +8,29 @@ import { useEffect, useState, useRef } from "react";
 import { motion } from "framer-motion-3d";
 
 export default function Experience(props) {
-	const mouse = {
-		x: useMotionValue(0),
-		y: useMotionValue(0),
-	};
+	// const mouse = {
+	// 	x: useMotionValue(0),
+	// 	y: useMotionValue(0),
+	// };
 
-	const smoothMouse = {
-		x: useSpring(mouse.x, { stiffness: 75, damping: 100, mass: 3 }),
-		y: useSpring(mouse.y, { stiffness: 75, damping: 100, mass: 3 }),
-	};
+	// const smoothMouse = {
+	// 	x: useSpring(mouse.x, { stiffness: 75, damping: 100, mass: 3 }),
+	// 	y: useSpring(mouse.y, { stiffness: 75, damping: 100, mass: 3 }),
+	// };
 
-	const manageMouse = (e) => {
-		const { innerWidth, innerHeight } = window;
-		const { clientX, clientY } = e;
-		const x = clientX / innerWidth;
-		const y = clientY / innerHeight;
-		mouse.x.set(x);
-		mouse.y.set(y);
-	};
+	// const manageMouse = (e) => {
+	// 	const { innerWidth, innerHeight } = window;
+	// 	const { clientX, clientY } = e;
+	// 	const x = clientX / innerWidth;
+	// 	const y = clientY / innerHeight;
+	// 	mouse.x.set(x);
+	// 	mouse.y.set(y);
+	// };
 
-	useEffect(() => {
-		window.addEventListener("mousemove", manageMouse);
-		return () => window.removeEventListener("mousemove", manageMouse);
-	}, []);
+	// useEffect(() => {
+	// 	window.addEventListener("mousemove", manageMouse);
+	// 	return () => window.removeEventListener("mousemove", manageMouse);
+	// }, []);
 
 	const [lightIntensity, setLightIntensity] = useState(40);
 	const [colorIntensity, setColorIntensity] = useState(0);
@@ -121,7 +121,7 @@ export default function Experience(props) {
 							roughness={roughness}
 							position={[-1.5, 0, 0]}
 							rotation={[0.2, 1.8, 1.259]}
-							mouse={smoothMouse}
+							// mouse={smoothMouse}
 							multiplier={4}
 						/>
 
@@ -131,7 +131,7 @@ export default function Experience(props) {
 							roughness={roughness}
 							position={[2, 0, 0]}
 							rotation={[-0.9, -0.5, -0.08]}
-							mouse={smoothMouse}
+							// mouse={smoothMouse}
 							multiplier={1}
 						/>
 					</group>
